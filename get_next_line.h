710566c16ba5b8ecec81b4cfbd54907a42d56aee
Kerem Apaydin <kapaydin@k1m04s06.42istanbul.com.tr>
2026-05-11 14:50:10 +0300

    Update: updated header file

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kapaydin <kapaydin@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: kapaydin <kapaydin@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:38:11 by kapaydin          #+#    #+#             */
-/*   Updated: 2026/05/09 15:38:12 by kapaydin         ###   ########.fr       */
+/*   Updated: 2026/05/11 14:49:35 by kapaydin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *str, int chr);
+char	*ft_mini_split(const char *str, int chr);
+
+#endif
