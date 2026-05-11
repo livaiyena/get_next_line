5bbbfb36919964d0e8f19f94f6e2fc7a1f514af2
Kerem Apaydin <kapaydin@k1m04s06.42istanbul.com.tr>
2026-05-11 14:52:11 +0300

    Update: some handlerss

--- get_next_line.c
+++ get_next_line.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kapaydin <kapaydin@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: kapaydin <kapaydin@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:38:08 by kapaydin          #+#    #+#             */
-/*   Updated: 2026/05/09 15:55:46 by kapaydin         ###   ########.fr       */
+/*   Updated: 2026/05/11 14:49:02 by kapaydin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
+#include "get_next_line.h"
+
+char	*get_from_remain(char	*remaining_data)
 {
-	
+
+}
+
+
+char	*get_next_line(int fd)
+{
+	static char	*remaining_data;
+
+	remaining_data = NULL;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 }
