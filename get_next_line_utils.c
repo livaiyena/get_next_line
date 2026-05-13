02f9d0a6ddb5d6172f1ceb29c3835be522ab1499
Kerem Apaydın <apaydinkerem18@gmail.com>
2026-05-14 02:18:53 +0300

    Update: revised strdup added strjoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kapaydin <kapaydin@student.42istanbul.com. +#+  +:+       +#+        */
+/*   By: kapaydin <kapaydin@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:38:04 by kapaydin          #+#    #+#             */
-/*   Updated: 2026/05/11 16:06:40 by kapaydin         ###   ########.fr       */
+/*   Updated: 2026/05/14 02:16:48 by kapaydin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,17 @@ size_t	ft_strlen(const char *str)
 char	*ft_strdup(const char *s1)
 {
 	char	*dest;
+	char	*d;
 	size_t	len;
 
 	len = ft_strlen(s1);
 	dest = (char *)malloc(sizeof(char) * (len + 1));
 	if (!dest)
 		return (NULL);
-	while (len)
-	{
-		dest[len] = s1[len];
-		len--;
-	}
+	d = dest;
+	while (*s1)
+		*d++ = *s1++;
+	*d = '\0';
 	return (dest);
 }
 
@@ -53,20 +53,25 @@ char	*ft_strchr(const char *str, int chr)
 	return ((char *)str);
 }
 
-char	*ft_mini_split(const char *str, int chr)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
-	int		size;
-	char	c;
-	char	*line;
+	char	*str;
+	char	*ptr;
+	size_t	i;
+	size_t	j;
 
-	size = 0;
-	c = (char)chr;
-	line = str;
-	while (str != c)
-	{
-		str++;
-		size++;
-	}
-	line[size + 1] = '\0';
-	return (ft_strdup(line));
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (i + j + 1));
+	if (!str)
+		return (NULL);
+	ptr = str;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = '\0';
+	return (str);
 }
